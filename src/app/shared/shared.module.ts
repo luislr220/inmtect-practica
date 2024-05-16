056e34a1d5026080c9a05cc06618318efe5bd4f1
luislr220 <largoramirezluisarmando@gmail.com>
2024-05-16 17:17:31 -0600

    Practica inmtec

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { MenuComponent } from './components/menu/menu.component';
+
+@NgModule({
+  declarations: [MenuComponent],
+  exports: [MenuComponent],
+  imports: [CommonModule, IonicModule],
+})
+export class SharedModule {}
