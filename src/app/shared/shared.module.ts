f75e3e4e6cb3215f517f6f81289bb758fbc94cb5
luislr220 <largoramirezluisarmando@gmail.com>
2024-05-17 13:41:35 -0600

    cambios xd

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { MenuComponent } from './components/menu/menu.component';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
   declarations: [MenuComponent],
   exports: [MenuComponent],
-  imports: [CommonModule, IonicModule],
+  imports: [CommonModule, IonicModule,RouterLink],
 })
 export class SharedModule {}
